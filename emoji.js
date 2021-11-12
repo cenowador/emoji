eea7e309b175a148bfd875599019afd2c5a363e3
cenowador <santos.jorge.augusto97@gmail.com>
2021-11-12 18:11:30 -0300

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -46,7 +46,13 @@ function loadEmojis(){
 		}
 	});
 }
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
 
-$(window).on("load", function() {
-	$('#emojis_whiteoverlay').hide();
-});
+if(!isSafari){
+	$(window).on("load", function() {
+		$('#emojis_whiteoverlay').hide();
+	});
+}
+else{
+	setTimeout(function(){$('#emojis_whiteoverlay').hide();}, 2000);
+}
